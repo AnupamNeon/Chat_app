27ddab544d04cd52084d93ba0f7dfd5c9c1fdd19
AnupamNeon <anupammaurya84377@gmail.com>
2025-10-06 14:00:04 +0530

    fixed axios.js file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+const SERVER_URL = import.meta.env.VITE_API_BASE_URL
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : SERVER_URL,
   withCredentials: true,
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,8 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "";
+const SERVER_URL = import.meta.env.VITE_API_BASE_URL
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : SERVER_URL;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
